7cc0896447ca47a5cfe961e8de1abbcb1f19e426
Ernesto Rodríguez Carámbula <ecarambula@gmail.com>
2024-02-23 13:12:04 +0000

    Crea barra de navegacion

--- portada/app.py
+++ portada/app.py
@@ -2,12 +2,20 @@ from shiny import ui, App
 
 # Interfase de usuario ----
 app_ui = ui.page_navbar(
-    ui.nav("Página 1"),
-    ui.nav("Página 2"),
+    ui.nav_panel("Página 1"),
+    ui.nav_panel("Página 2"),
+    ui.nav_control(ui.a("MINCyT", href = "https://www.argentina.gob.ar/ciencia")),
+    ui.nav_menu(
+        "Más",
+        ui.nav_control(ui.a("Financiamiento", href = "https://www.argentina.gob.ar/ciencia/financiamiento")),
+        ui.nav_control(ui.a("Publicaciones", href = "https://www.argentina.gob.ar/ciencia/publicaciones-cyt"))
+        ),
     title = ui.row(
-        ui.column(3, ui.img(src = "https://aluno.analisemacro.com.br/wp-content/uploads/dlm_uploads/2023/05/logo_am_45.png")),
+        ui.column(3, ui.img(src = "/workspaces/myncit_dash/portada/Mincyt.png")),
         ui.column(9, "PFI 2023")
-    )
+    ),
+    bg = "blue",
+    inverse = True
 )
 
 
